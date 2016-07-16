95f14797d6c2bfdd48cd80eaa252bffb0dc33947
Emory Merryman <emory.merryman@gmail.com>
2016-07-16 11:28:59 +0000

    we can not export from within inside a parenthesis

--- cloud9.sh
+++ cloud9.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 export CANONICAL=${1} &&
-(
-    ( [ 1 == $# ] && export ORIGIN=${CANONICAL} ) ||
-    ( [ 2 == $# ] && export ORIGIN=${2} )
-) &&
+export ORIGIN=${2} &&
 docker-compose up -d &&
 docker-compose ps &&
 true
